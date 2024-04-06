73bbb4c1f8b55c155f7b0d454eea9ebc84b4d51d
Alejandra <abigailmolina201@gmail.com>
2024-04-05 23:42:29 -0600

    Se realizaron cambios nuevos

--- Js/database.js
+++ Js/database.js
@@ -96,13 +96,15 @@ function openDatabase() {
         }
         //Almacen para examenes de Orina
         if (!db.objectStoreNames.contains('examenesOrina')) {
-            const examenOrinaStore = db.createObjectStore('examenesOrina', { keyPath: 'idExamenOrina', autoIncrement: true });
-            examenOrinaStore.createIndex('idPaciente', 'idPaciente', { unique: true});
+            const examenOrinaStore = db.createObjectStore('examenesOrina', { keyPath: 'id', autoIncrement: true })
+            examenOrinaStore.createIndex('idPaciente', 'idPaciente', { unique: true });
+            examenOrinaStore.createIndex('consultaId', 'consultaId', { unique: true });
             examenOrinaStore.createIndex('glucosa', 'glucosa', { unique: false });
             examenOrinaStore.createIndex('eritrocitos', 'eritrocitos', { unique: false });
             examenOrinaStore.createIndex('color', 'color', { unique: false });
             examenOrinaStore.createIndex('leucocitos', 'leucocitos', { unique: false });
         }
+        
         
         //Almacen para historial de consultas médicas
         // if (!db.objectStoreNames.contains('historialConsultas')) {
