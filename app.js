4c3b53bd3690c41063775e3923557eb6e68b38ef
AdonayXX <emdiazdu@est.utn.ac.cr>
2024-04-21 10:34:09 -0600

    Cambios para despliegue de página

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.get('/auth/google/callback',
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () =>{
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
